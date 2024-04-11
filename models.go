62b78f0131dd4ff475af2ccee401a574edb3847d
Anna Hoffa <60316298+annahoffa@users.noreply.github.com>
2024-04-11 23:47:10 +0200

    [feat] Add nginx healthcheck EP (#1)

--- models.go
+++ models.go
@@ -3,12 +3,13 @@ package main
 type Domain struct {
 	Name                  string `json:"name"`
 	ContainerName         string `json:"container_name"`
-	IP                    string `json:"ip"`
+	Ip                    string `json:"ip"`
 	HttpEnabled           bool   `json:"http_enabled"`
 	HttpsEnabled          bool   `json:"https_enabled"`
 	HttpWebsocketsEnabled bool   `json:"http_websockets_enabled"`
 }
 
 type Config struct {
-	Domains []Domain `json:"domains"`
+	Domains              []Domain `json:"domains"`
+	HealthcheckAllowedIp string   `json:"healthcheck_allowed_ip"`
 }
